d46e10c048429260fded7fd39e5f6d98e9812482
hyx <hanyuxuan@longi.com>
2024-03-27 16:20:02 +0800

    use wallpaper

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
+use chrono::TimeDelta;
+
 
 const URL: &str = "https://himawari8.nict.go.jp/img/D531106/1d/550";
 
 fn main() {
     
-    let date = chrono::Utc::now().format("%Y/%m/%d/%H");
+    let date = chrono::Utc::now().checked_sub_signed(TimeDelta::try_hours(1).unwrap()).unwrap();
+    let date = date.format("%Y/%m/%d/%H").to_string();
+
+
+    
 
     let url = format!("{}/{}{}", URL, date, "0000_0_0.png");
     print!("Downloading image from: {}", url);
 
-    // get the picture and store it in memory
-    let image_bytes = reqwest::blocking::get(&url)
-        .unwrap()
-        .error_for_status()
-        .unwrap()
-        .bytes()
-        .unwrap();
 
-    let img = image::load_from_memory(&image_bytes).unwrap();
+    
+    wallpaper::set_mode(wallpaper::Mode::Fit).unwrap();
+    wallpaper::set_from_url(&url).unwrap();
 
 
 
